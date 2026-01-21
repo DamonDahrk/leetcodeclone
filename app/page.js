f06729d968cd2c51c68fc3eb9d6617368b7ca377
Sayantan Chowdhury <shyuhaspivot@gmail.com>
2026-01-21 16:19:12 +0530

    Updated the button text in app/page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className ="flex flex-col items-center justify-center h-screen"><Button>
-      Click Me
+      Click Me here
       </Button></div>
   );
 }
