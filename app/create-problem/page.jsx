22ae676998447bda3c739088f7025a7c909754c1
Sayantan Chowdhury <shyuhaspivot@gmail.com>
2026-01-23 12:13:31 +0530

    still working, create problem form soon

--- app/create-problem/page.jsx
+++ app/create-problem/page.jsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components/ui/button';
+import { ModeToggle } from '@/components/ui/mode-toggle';
 import { currentUserRole } from '@/modules/auth/actions';
+import CreateProblemForm from '@/modules/problems/components/create-problem-form';
 import { currentUser } from '@clerk/nextjs/server'
 import { UserRole } from '@prisma/client';
 import { ArrowLeft } from 'lucide-react';
@@ -15,7 +17,7 @@ const CreateProblemPage = async () => {
     }
 
   return (
-<section className="flex flex-col items-center justify-center container mx-4 my-4">
+<section className="flex flex-col items-center justify-center mx-4 my-4">
   <div className="flex flex-row justify-between items-center w-full">
     
     <Link href="/"> 
@@ -23,7 +25,11 @@ const CreateProblemPage = async () => {
         <ArrowLeft className = "size-4"/>
     </Button>
     </Link>
+    <h1 className = "text-3xl font-bold text-amber-400"
+    > Welcome {user?.firstName}! Create a Problem</h1>
+    <ModeToggle />
   </div>
+  <CreateProblemForm/>
 </section>
 )
 }
---
+++ modules/problems/components/create-problem-form.jsx
@@ -0,0 +1,43 @@
+"use client";
+import { Editor } from "@monaco-editor/react";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Plus,
+  Trash2,
+  Code2,
+  FileText,
+  Lightbulb,
+  BookOpen,
+  CheckCircle2,
+  Download,
+} from "lucide-react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
+
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+const CreateProblemForm = () => {
+  return (
+    <div>
+      CreateProblemForm
+    </div>
+  )
+}
+
+export default CreateProblemForm
